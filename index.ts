e5af66aa41cc4d474fdfa10e88c5e529738def49
Hassan <142681707+Hassan3108huzaifa@users.noreply.github.com>
2024-03-23 01:29:43 +0500

    creating loop

--- index.ts
+++ index.ts
@@ -1,20 +1,28 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 import inquirer from "inquirer";
 
 console.log("Welcome to my number guessing game")
 
-const randomNumber = Math.floor(Math.random() * 6 + 1);
+async function guessingGame() {
+  while (true) {
+    const randomNumber = Math.floor(Math.random() * 6 + 1);
 
-const answere = await inquirer.prompt([
-  {
-    name: "userGuessedNumber",
-    type: "number",
-    message: "Please guess a number between 1 to 6: ",
-  },
-]);
-if (answere.userGuessedNumber == randomNumber) {
-  console.log("Congratulations! you guessed a right number");
-} else {
-  console.log("you guessed a wrong number");
+    const answer = await inquirer.prompt([
+      {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1 to 6: ",
+      },
+    ]);
+
+    if (answer.userGuessedNumber == randomNumber) {
+      console.log("Congratulations! You guessed the right number");
+      break; // Exit the loop if the guess is correct
+    } else {
+      console.log("You guessed the wrong number. Please try again.");
+    }
+  }
 }
+
+guessingGame();
